c4faccce99c76afd5068ab620fd4126d94a0ec05
danielahn <93danielahn@gmail.com>
2019-08-14 16:53:43 -0400

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,10 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+
+
+-- since we are in a .sql file we can just create the table 
+CREATE TABLE IF NOT EXISTS pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT
+);
